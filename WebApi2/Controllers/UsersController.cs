cdaf6e1533491ac1e03675f67208d489d79d7fe5
Hamed <motaghee@hotmail.com>
2019-07-13 15:27:44 +0430

    980422-saipa

--- WebApi2/Controllers/UsersController.cs
+++ WebApi2/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using WebApi2.Models;
@@ -15,11 +16,11 @@ namespace WebApi2.Controllers
     {
         private List<User> users = new List<User>
     {
-        new User { id = 1, name = "Hamed1"},
-        new User { id = 2, name = "Hadi2"},
-        new User { id = 3, name = "Reza3"},
-        new User { id = 4, name = "Omid4"},
-        new User { id = 5, name = "Saeed5"}
+        new User { id = 1, Fname = "Hamed1"},
+        new User { id = 2, Fname = "Hadi2"},
+        new User { id = 3, Fname = "Reza3"},
+        new User { id = 4, Fname = "Omid4"},
+        new User { id = 5, Fname = "Saeed5"}
     };
         // GET: api/Users
         //[ResponseType(typeof(IEnumerable<User>))]
@@ -48,16 +49,42 @@ namespace WebApi2.Controllers
             User u = new User();
             u.id = id;
             if ((ds != null) && (ds.Tables[0] != null) && (ds.Tables[0].Rows.Count > 0)) 
-                u.name= ds.Tables[0].Rows[0][0].ToString();
+                u.Fname= ds.Tables[0].Rows[0][0].ToString();
             else
-                u.name= "Not Found";
+                u.Fname= "Not Found";
             //
             //u.id = 6;
             //u.name = "Ali";
             //users.Add(u);
             return u;
         }
+        // POST: api/Login
+        [HttpPost]
+        public User Post([FromBody] User U)
+        {
+            // int i =U.id;
+            if ((U != null) && (U.Macaddress == "48:13:7e:11:d7:1f"))
+            {
 
-        
-}
+                User U2 = new User();
+                U2.Username = "1000861";
+                U2.id = 4314;
+                U2.Fname = "hamiReza";
+                U2.Lname = "MT";
+                return U2;
+            }
+            else
+            {
+                User U2 = new User();
+                U2.Username = "InvalidMac";
+                U2.id = 0;
+                U2.Fname = "InvalidMac";
+                U2.Lname = "InvalidMac";
+                return U2;
+            }
+        }
+
+
+
+    }
 }
--- WebApi2/Models/User.cs
+++ WebApi2/Models/User.cs
@@ -8,6 +8,12 @@ namespace WebApi2.Models
     public class User
     {
         public decimal id { get; set; }
-        public string name { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Macaddress { get; set; }
+
+
     }
 }
